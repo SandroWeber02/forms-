7957e25cc4deca6f0e1d4f901719fae727c381c1
Sandro Weber <124099614+SandroWeber02@users.noreply.github.com>
2024-04-09 15:57:58 -0300

    Add files via upload

---
+++ FormAjustado/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_ValidaCPF : Form
+    {
+        public Frm_ValidaCPF()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_ValidaCPF_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        public bool Valida(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+
+        private void Msk_CPF_MaskinputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            bool validaCPF = false;
+            validaCPF = Valida(Msk_CPF.Text);
+            if (validaCPF == true)
+            {
+                Lbl_Resultado.Text = "CPF VALIDO";
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+            else
+            {
+                Lbl_Resultado.Text = "CPF INVALIDO";
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+
+          
+        }
+    }
+}
